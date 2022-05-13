0b179369f1c32f6db03159943443de3cf8483626
coderbhaai <amit.khare588@gmail.com>
2022-05-13 14:25:28 +0530

    `commit`

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Livewire\Pages\Home;
+use App\Http\Livewire\Pages\About;
+use App\Http\Livewire\Pages\Customersupport;
+use App\Http\Livewire\Pages\Faq;
+use App\Http\Livewire\Pages\Fourofour;
+use App\Http\Livewire\Pages\Thankyou;
+use App\Http\Livewire\Pages\Tnc;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Livewire\Ecom\Cart;
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
+
+
+Route::get("/", Home::class)->name('home');
+Route::get("/about", About::class)->name('about');
+Route::get("/customer-support", Customersupport::class)->name('customerSupport');
+Route::get("/faq", Faq::class)->name('faq');
+Route::get("/404", Fourofour::class)->name('404');
+Route::get("/thank-you", Thankyou::class)->name('thankyou');
+Route::get("/terms-and-conditions", Tnc::class)->name('tnc');
+
+Route::get("/cart", Cart::class)->name('cart');
